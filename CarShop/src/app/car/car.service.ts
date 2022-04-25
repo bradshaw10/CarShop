0f3cb3b39efca2f87e7db91e128d8b0c3688d392
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-04-25 18:59:43 +0400

    Update car.service.ts

--- CarShop/src/app/car/car.service.ts
+++ CarShop/src/app/car/car.service.ts
@@ -1,7 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Car } from './car';
+import { map } from 'rxjs/operators'
 
 @Injectable({
     providedIn: 'root'
 })
 export class CarService {
+
+    baseUrl = 'http://localhost/api';
+
+    constructor(private http: HttpClient) { }
+
+    getAllCars() {
+        return this.http.get('${this.baseUrl}/list').pipe(
+            map((res: any) => {
+                return res['data']
+            })
+        )
+    }
 }
